a7648a61329d3b3a911491eb074649c0f1b27f54
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-02-11 10:07:27 -0300

    Retorna uma string com o link de seu avatar no github

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -4,8 +4,8 @@ import requests
 def buscar_avatar(usuario):
     """
     Busca o avatar de um usuário no github
-    :param usuario str: com o nome do usuário no github
-    :return str com o link do usuário avatar
+    :param usuário str: com o nome do usuário no github
+    :return str com o link do usuário
     """
     url =f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
