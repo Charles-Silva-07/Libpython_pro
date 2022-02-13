19d5b251486bcf67067ad742d342275217dc1b04
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-02-13 11:57:17 -0300

    Test de validação de email

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,27 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviardor = Enviador()
+    assert enviardor is not None
+
+
+@pytest.mark.parametrize('destinatario', ['charlleshp@hotmail.com','edvaniavieira2021@gmail.com'])
+def test_remetente(destinatario):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        destinatario,
+        'charllesrocklp@gmail.com',
+        'Curso python pro',
+        'prairie turma'
+    )
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize('remetente', ['charlleshphotmail.com', 'edvaniavieira2021gmail.com', ''])
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(remetente, 'charllesrocklp@gmail.com', 'Curso python pro', 'prairie turma')
