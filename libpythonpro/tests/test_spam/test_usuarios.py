7b7757108da0c98a2c0c69f958fb599290e0389a
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-02-13 21:01:39 -0300

    Erro

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,23 +1,4 @@
-import pytest
-
-from libpythonpro.tests.test_spam.db import Conexao
-from libpythonpro.tests.test_spam.modelos import Usuario
-
-
-@pytest.fixture
-def conexao():
-    #setup
-    conexao_obj = Conexao()
-    yield conexao_obj
-    # Tear Down
-    conexao_obj.fechar()
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sesao()
-    yield sessao_obj
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
+from libpythonpro.spam.modelos import Usuario
 
 
 def teste_salvar_usuario(sessao):
@@ -26,7 +7,7 @@ def teste_salvar_usuario(sessao):
     assert isinstance(usuario.id, int)
 
 
-def teste_listar_usuario(conexao, sessao):
+def teste_listar_usuario(sessao):
     usuarios = [Usuario(nome='Charles'), Usuario(nome='Luciano')]
     for usuario in usuarios:
         sessao.salvar(usuario)
